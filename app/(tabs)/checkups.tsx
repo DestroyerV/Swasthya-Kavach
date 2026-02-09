88e48e2abc0d89276c26155938d5897d6aec4588
DestroyerV <mishravaibhav12321@gmail.com>
2026-02-09 22:26:02 +0530

    feat: remove demo expo project

--- app/(tabs)/checkups.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function CheckupsScreen() {
-  return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-2xl font-bold">Checkup History</Text>
-    </View>
-  );
-}
